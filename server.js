0a76a2d3eac27818a327d41e5153219374bf2aa5
marvin-showkat <marvinmaster17@gmail.com>
2026-02-12 16:11:34 +0530

    Updated backend

--- server.js
+++ server.js
@@ -166,7 +166,7 @@ Return ONLY valid JSON.
 }
 
 // ---------------- SERVER ----------------
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on http://localhost:${PORT}`);
